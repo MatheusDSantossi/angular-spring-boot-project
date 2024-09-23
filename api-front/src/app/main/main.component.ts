a27cbde52e85d707ee9a1932bb5236022c500a73
Matheus D. Santosr <matheus1819.santos20@gmail.com>
2024-09-23 18:08:55 -0300

    Manipulando e editando os arquivos main.html, .css e .ts

--- api-front/src/app/main/main.component.ts
+++ api-front/src/app/main/main.component.ts
@@ -1,12 +1,15 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
-
+  
+  // Variable for buttons visibility 
+  btnRegister:boolean = true;
 }
